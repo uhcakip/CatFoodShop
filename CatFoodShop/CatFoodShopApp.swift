be69b9873947a4e0251e25ae64389512ba749cd1
yunac <yunac.owob@gmail.com>
2024-11-13 13:32:17 +0800

    chore: init

---
+++ CatFoodShop/CatFoodShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFoodShopApp.swift
+//  CatFoodShop
+//
+//  Created by Yuna Chou on 2024/11/13.
+//
+
+import SwiftUI
+
+@main
+struct CatFoodShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
